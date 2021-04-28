a7a8abf340785e4594af4d0f121555f9c20701d0
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-28 18:54:15 +0900

    sseでcurl接続確認

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 require('dotenv').config();
+import Express, { Request, Response } from 'express';
 
 const Twitter = require('twitter');
 const client = new Twitter({
@@ -8,8 +9,29 @@ const client = new Twitter({
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
+const stream = client.stream('statuses/filter', { track: 'こんにちは' });
 
-const stream = client.stream('statuses/filter', { track: 'hello' });
-stream.on('data', (event: any) => {
-    console.log(event && event.text);
+
+
+const app: Express.Application = Express();
+app.get('/streams', (req: Request, res: Response) => {
+
+    //sseでの接続を宣言
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        Connection: 'keep-alive',
+    });
+
+    stream.on('data', (event: any) => {
+        if (event) {
+            res.write(JSON.stringify({ url: event.profile_background_image_url_https });
+        }
+    });
+
 });
+
+app.listen(3000, () => {
+    console.log("listening start...");
+});
+
