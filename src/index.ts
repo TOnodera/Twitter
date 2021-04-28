4dddc4be7cb9325fc4917e01f0eb0b7433486780
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-29 05:03:35 +0900

    100件キャッシュ持たせる

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { app } from './server';
-import { corsSetting } from './setting';
 import { Request, Response } from 'express';
 import moment = require('moment');
 import { stream } from './stream';
 
+//インメモリでメッセージを100件保持しておく
+const messages: Message[] = [];
 
-app.get('/streams', corsSetting, (req: Request, res: Response) => {
+
+
+app.get('/streams', (req: Request, res: Response) => {
 
     //sseでの接続を宣言
     res.writeHead(200, {
@@ -15,19 +18,34 @@ app.get('/streams', corsSetting, (req: Request, res: Response) => {
     });
 
     stream.on('data', (event: any) => {
+
         if (event) {
+
             const datetime = moment(new Date(event.created_at));
             const date = datetime.format("YYYY-MM-DD");
             const time = datetime.format("h:mm:ss");
             const dayOfMonth = datetime.format("MM/DD");
+
             if (event.user) {
-                res.write("data: " + JSON.stringify({ name: event.user.name, text: event.text, src: event.user.profile_background_image_url_https, date: date, time: time, dayOfMonth: dayOfMonth, urls: event.entities.urls }));
+
+                const message: Message = { name: event.user.name, text: event.text, src: event.user.profile_background_image_url_https, date: date, time: time, dayOfMonth: dayOfMonth, urls: event.entities.urls };
+                res.write("data: " + JSON.stringify(message));
                 res.write("\n\n");//sseのフォーマットに沿って１つのメッセージの終わりに改行２ついれる
+
+                messages.unshift(message);
+                if (messages.length > 100) {
+                    messages.pop();
+                }
+
             }
 
         }
     });
 
+});
+
+app.get('/start', (req: Request, res: Response) => {
+    res.json(messages);
 });
 
 app.listen(process.env.APP_SERVER_PORT, () => {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import Express from 'express';
+import { corsSetting } from './setting';
 
 const app: Express.Application = Express();
+
+app.use(corsSetting);
 
 export {
     app
