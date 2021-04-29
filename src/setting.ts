839dc072cf69176922b746893ebb37e16cda01e4
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-29 22:39:25 +0900

    修正

--- src/setting.ts
+++ src/setting.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 import cors from 'cors';
 
 const origin = process.env.NODE_ENV == 'prod'
-    ? `${process.env.WEB_SERVER_URL}:${process.env.WEB_SERVER_PORT}`
+    ? `${process.env.WEB_SERVER_URL}`
     : 'http://localhost';
 
 const corsOptions = {
