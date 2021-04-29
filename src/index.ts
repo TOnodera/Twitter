0e5a8c9faba4ab17a357f961f2013370795d66ef
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-29 16:13:01 +0900

    コード整理

--- src/index.ts
+++ src/index.ts
@@ -2,46 +2,9 @@ import { app } from './server';
 import { Request, Response } from 'express';
 import moment = require('moment');
 import { stream } from './stream';
+import Messages from './messages';
 
-//ユーザー定義タイプガード
-const isImplmentsMessage = (arg: any): arg is Message => {
-    return arg != null &&
-        typeof arg.name == 'string' &&
-        typeof arg.text == 'string' &&
-        typeof arg.src == 'string' &&
-        typeof arg.date == 'string' &&
-        typeof arg.time == 'string' &&
-        typeof arg.dayOfMonth == 'string' &&
-        typeof arg.urls == 'object';
-}
 
-//インメモリでメッセージを100件保持しておく
-const messages: Message[] = [];
-
-//Messageの変更監視するためにプロキシ
-const messagesProxy = {
-    set: (message: Message) => {
-        messages.unshift(message);
-        messageCallback(message);
-        if (messages.length >= 100) {
-            messages.pop();
-        }
-    }
-};
-
-//ハンドラ
-let handler = (message: Message) => { };
-
-const notice = (message: Message) => {
-    handler(message);
-}
-
-//MessageProxyがデータ受け取った時に渡すためのコールバック
-const messageCallback = (message: Message) => {
-    notice(message);
-};
-
-//新しいタイムラインをどんどん配列に入れていく
 stream.on('data', (event: any) => {
 
     if (event) {
@@ -56,14 +19,13 @@ stream.on('data', (event: any) => {
             const src: string = event.user.profile_background_image_url_https == 'unkown' ? '' : event.user.profile_background_image_url_https
             const message: Message = { id: event.id, name: event.user.name, text: event.text, src: src, date: date, time: time, dayOfMonth: dayOfMonth, urls: event.entities.urls };
 
-            messagesProxy.set(message);
+            //新たに受信したメッセージをMessagesオブジェクト内の配列に突っ込む
+            Messages.set(message);
 
         }
 
     }
 });
-
-
 
 
 app.get('/streams', (req: Request, res: Response) => {
@@ -75,20 +37,20 @@ app.get('/streams', (req: Request, res: Response) => {
         Connection: 'keep-alive',
     });
 
-    //Messageを監視して変更があったらデータ送信
-    handler = (message: Message) => {
-        //console.log(message.id_str, message.name, moment(new Date()).format('YYYY-MM-DD h:mm:ss'));
+    //Message内の配列を監視して変更があったらデータ送信
+    Messages.handle((message: Message) => {
         res.write("data: " + JSON.stringify(message));
         res.write("\n\n");
-    };
-
+    });
 
 });
 
+//ページに最初に訪問した時に１度だけ実行される。サーバーのインメモリのキャッシュから最大１００件のデータを返す
 app.get('/start', (req: Request, res: Response) => {
-    res.json(messages);
+    res.json(Messages.get());
 });
 
+//サーバー起動
 app.listen(process.env.APP_SERVER_PORT, () => {
     console.log("listening start...");
 });
---
+++ src/messages.ts
@@ -0,0 +1,30 @@
+class Messages {
+
+    //インメモリでメッセージをMAX_CACHE_NUM件保持しておく
+    private static messages: Message[] = [];
+    private static MAX_CACHE_NUM = 100;
+    private static handler: (message: Message) => void;
+
+    static set(message: Message) {
+        this.messages.unshift(message);
+        this.messageCallback(message);
+        if (this.messages.length >= this.MAX_CACHE_NUM) {
+            this.messages.pop();
+        }
+    }
+
+    static handle(handler: (message: Message) => void) {
+        this.handler = handler;
+    }
+
+    private static messageCallback(message: Message) {
+        this.handler(message);
+    }
+
+    static get(): Message[] {
+        return this.messages;
+    }
+
+}
+
+export default Messages;
