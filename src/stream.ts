5fc9a0a19ab59426436a12c9a0dcb2a0e063119e
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-05-01 01:36:46 +0900

    修正

--- src/stream.ts
+++ src/stream.ts
@@ -14,25 +14,23 @@ const stream = client.stream('statuses/filter', { track: process.env.TRACK_WORDS
 
 function get(): Promise<Message> {
     return new Promise((resolve) => {
-        if (stream.listenerCount('data') == 0) {
-            stream.once('data', (event: any) => {
+        stream.once('data', (event: any) => {
 
-                const datetime = moment(new Date(event.created_at));
-                const date = datetime.format("YYYY-MM-DD");
-                const time = datetime.format("h:mm:ss");
-                const dayOfMonth = datetime.format("MM/DD");
+            const datetime = moment(new Date(event.created_at));
+            const date = datetime.format("YYYY-MM-DD");
+            const time = datetime.format("h:mm:ss");
+            const dayOfMonth = datetime.format("MM/DD");
 
-                if (event.user) {
+            if (event.user) {
 
-                    const src: string = event.user.profile_background_image_url_https == 'unkown' ? '' : event.user.profile_background_image_url_https
-                    const message: Message = { id: event.id, name: event.user.name, text: event.text, src: src, date: date, time: time, dayOfMonth: dayOfMonth, urls: event.entities.urls };
+                const src: string = event.user.profile_background_image_url_https == 'unkown' ? '' : event.user.profile_background_image_url_https
+                const message: Message = { id: event.id, name: event.user.name, text: event.text, src: src, date: date, time: time, dayOfMonth: dayOfMonth, urls: event.entities.urls };
 
-                    //新たに受信したメッセージを通知
-                    resolve(message);
+                //新たに受信したメッセージを通知
+                resolve(message);
 
-                }
-            });
-        }
+            }
+        });
     });
 }
 
