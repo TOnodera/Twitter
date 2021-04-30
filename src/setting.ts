76dd48c6a49bbec17a011d4b34647fd96bb6a2ec
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-30 12:24:02 +0900

    ポート番号つけた

--- src/setting.ts
+++ src/setting.ts
@@ -1,9 +1,9 @@
 require('dotenv').config();
 import cors from 'cors';
 
-const origin = process.env.NODE_ENV == 'prod'
+const origin = process.env.NODE_ENV == 'production'
     ? `${process.env.WEB_SERVER_URL}`
-    : 'http://localhost';
+    : `http://localhost:${process.env.APP_SERVER_PORT}`;
 
 const corsOptions = {
     origin: origin,
