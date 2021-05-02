f0f2bd7997f78f6fcb64cd7970d161a0618270ce
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-05-02 11:37:33 +0900

    整理

---
+++ src/launch.ts
@@ -0,0 +1,37 @@
+import { app } from './server';
+import { Request, Response } from 'express';
+import { generator } from './stream';
+import Messages from './Messages';
+
+const launch = () => {
+    app.get('/streams', async (req: Request, res: Response) => {
+
+
+        //sseでの接続を宣言
+        res.writeHead(200, {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+        });
+
+        for await (const message of generator()) {
+            Messages.set(message); //キャッシュ用
+            res.write("data: " + JSON.stringify(message));
+            res.write("\n\n");
+        }
+
+    });
+
+    //ページに最初に訪問した時に１度だけ実行される。サーバーのインメモリのキャッシュから最大１００件のデータを返す
+    app.get('/start', (req: Request, res: Response) => {
+        res.json(Messages.cache());
+    });
+
+    //サーバー起動
+    app.listen(process.env.APP_SERVER_PORT, () => {
+        console.log("listening start...");
+    });
+}
+
+export default launch;
+
--- src/utility.ts
+++ src/utility.ts
@@ -1,5 +1,6 @@
 import { Logger } from "logger";
 
 const logger = new Logger();
+const sleep = (s: number) => new Promise(resolve => setTimeout(resolve, s * 1000));
 
-export { logger }
+export { logger, sleep }
