6494b07f45fdcf5be78c02a6dc8ed521f6bf30ce
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-05-04 16:25:07 +0000

    クラスタ化一旦やめる

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
 import launch from "./launch";
 import stream from "./stream";
 
-/*
-//クラスタ化実行してサーバ
-if (clustor.isMaster) {
-    for (let i = 0; i < 1; i++) {
-        clustor.fork();
-    }
-} else {
-    launch(stream);
-}
-*/
-
 launch(stream);
