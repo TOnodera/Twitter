dc4ea6773049902b3d8780dc7d4a37f81c4430b4
Takeshi <t.onodera.1.2.1.5@gmail.com>
2021-06-13 15:48:12 +0000

    ジェネレーターの書き方変更

--- src/generator.ts
+++ src/generator.ts
@@ -1,38 +1,28 @@
+import events from "events";
 import moment from "moment";
+import EventEmitter from "node:events";
 import { logger } from "./utility";
 
-function get(stream: any): Promise<Message> {
-    return new Promise((resolve, reject) => {
-        stream.once('data', (event: any) => {
+async function* generator(stream: EventEmitter): AsyncGenerator<Message>{
+    const iterable = events.on(stream,'data');
+    for await (const event of iterable){
+        const datetime = moment(new Date(event.created_at));
+        const date = datetime.format("YYYY-MM-DD");
+        const time = datetime.format("h:mm:ss");
+        const dayOfMonth = datetime.format("MM/DD");
 
-            const datetime = moment(new Date(event.created_at));
-            const date = datetime.format("YYYY-MM-DD");
-            const time = datetime.format("h:mm:ss");
-            const dayOfMonth = datetime.format("MM/DD");
+        if (event.user) {
+            const src: string = event.user.profile_background_image_url_https == 'unkown' ? '' : event.user.profile_background_image_url_https
+            const message: Message = { id: event.id, name: event.user.name, text: event.text, src: src, date: date, time: time, dayOfMonth: dayOfMonth, urls: event.entities.urls };
 
-            if (event.user) {
-
-                const src: string = event.user.profile_background_image_url_https == 'unkown' ? '' : event.user.profile_background_image_url_https
-                const message: Message = { id: event.id, name: event.user.name, text: event.text, src: src, date: date, time: time, dayOfMonth: dayOfMonth, urls: event.entities.urls };
-
-                //新たに受信したメッセージを通知
-                resolve(message);
-            } else {
-                reject(event);
-            }
-        });
-    });
-}
-
-async function* generator(stream: any) {
-    while (true) {
-        try {
-            yield await get(stream);
-        } catch (e) {
+            //新たに受信したメッセージを通知
+            yield message;
+        }else{
             logger.debug("event.userが無いのでエラーを投げました");
         }
     }
 }
+
 
 
 export { generator };
